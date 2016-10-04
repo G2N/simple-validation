86f46ac30b6f9e1693e50501ed2bfecdaa699042
Guillaume Denormandie <denormandie.g@gmail.com>
2016-10-04 16:48:39 +0200

    Bugfix
    Was broken when used on multiple forms...

--- js/simplevalidation.js
+++ js/simplevalidation.js
@@ -13,48 +13,46 @@
 })(function () {
 	"use strict";
 
-    var form;
-
-    /**
-     * Removes the validity class on the element
-     */
-    function resetValidityClass(evt) {
-        var input = evt.target;
-        input.classList.remove('invalid');
-    }
-
-    /**
-     * Adds a validity class on a element if needed
-     */
-    function addValidityClass(evt) {
-        var input = evt.target;
-        if(!input.validity.valid) {
-            input.classList.add('invalid');
-        }
-    }
-
-    function validateMatchingField(evt) {
-        var input = evt.target,
-            selector = '[name='+input.dataset.validationMatches+'], #'+input.dataset.validationMatches,
-            message = input.dataset.validationMatchesMessage || 'This field should match',
-            match = form.querySelector(selector);
-
-        if(input.value !== match.value) {
-            input.setCustomValidity(message);
-        }
-        else {
-            input.setCustomValidity('');
-        }
-    }
 
     function SimpleValidation(el) {
         if(!(el && el.nodeType && el.nodeType === 1)) {
             throw 'SimpleValidation `el` must be an HTMLElement';
         }
 
-        form = el;
+        var form = el, 
+			inputs = el.querySelectorAll('[required]');
 
-        var inputs = el.querySelectorAll('[required]');
+		/**
+		* Removes the validity class on the element
+		*/
+		function resetValidityClass(evt) {
+			var input = evt.target;
+			input.classList.remove('invalid');
+		}
+
+		/**
+		* Adds a validity class on a element if needed
+		*/
+		function addValidityClass(evt) {
+			var input = evt.target;
+			if(!input.validity.valid) {
+				input.classList.add('invalid');
+			}
+		}
+
+		function validateMatchingField(evt) {
+			var input = evt.target,
+			selector = '[name='+input.dataset.validationMatches+'], #'+input.dataset.validationMatches,
+			message = input.dataset.validationMatchesMessage || 'This field should match',
+			match = form.querySelector(selector);
+
+			if(input.value !== match.value) {
+				input.setCustomValidity(message);
+			}
+			else {
+				input.setCustomValidity('');
+			}
+		}
 
         for (var i = 0; i < inputs.length; i++) {
             inputs[i].addEventListener('focus', resetValidityClass);
